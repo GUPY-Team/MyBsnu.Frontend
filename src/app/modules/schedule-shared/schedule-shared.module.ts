f356b7ada0571a5e549299b4817a931cde9768b8
neistow <glebchernigin236@gmail.com>
2021-09-24 23:11:43 +0300

    quickfix

---
+++ src/app/modules/schedule-shared/schedule-shared.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'app/modules/shared/shared.module';
+import {
+    ScheduleBodyComponent,
+    ScheduleCurrentDateComponent,
+    ScheduleFiltersComponent,
+    ScheduleItemComponent
+} from 'app/modules/schedule-shared/components';
+
+
+@NgModule({
+    declarations: [
+        ScheduleBodyComponent,
+        ScheduleItemComponent,
+        ScheduleCurrentDateComponent,
+        ScheduleFiltersComponent,
+    ],
+    exports: [
+        ScheduleBodyComponent,
+        ScheduleItemComponent,
+        ScheduleCurrentDateComponent,
+        ScheduleFiltersComponent,
+    ],
+    imports: [
+        SharedModule
+    ]
+})
+export class ScheduleSharedModule {
+}
