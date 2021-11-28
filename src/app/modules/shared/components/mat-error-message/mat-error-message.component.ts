37c442b361878cab9b1780997e1631b0cf67b415
neistow <glebchernigin236@gmail.com>
2021-11-28 20:51:47 +0200

    Update mat-error-message

--- src/app/modules/shared/components/mat-error-message/mat-error-message.component.ts
+++ src/app/modules/shared/components/mat-error-message/mat-error-message.component.ts
@@ -1,9 +1,9 @@
 import { AfterViewInit, Component, Injector, OnDestroy } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, EMPTY, merge, Observable, Subject } from 'rxjs';
 import { MatFormField, MatFormFieldControl } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { takeUntil } from 'rxjs/operators';
-import { ValidationErrors } from '@angular/forms';
+import { FormGroupDirective, ValidationErrors } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 
 interface ControlError {
@@ -34,7 +34,14 @@ export class MatErrorMessageComponent implements AfterViewInit, OnDestroy {
 
     public ngAfterViewInit(): void {
         this.inputRef = this.injector.get(MatFormField)._control;
-        this.inputRef.ngControl?.statusChanges?.pipe(
+
+        const valueChanges = this.inputRef.ngControl?.valueChanges ?? EMPTY;
+        const ngSubmit = this.injector.get(FormGroupDirective, null)?.ngSubmit ?? EMPTY;
+
+        merge(
+            valueChanges,
+            ngSubmit
+        ).pipe(
             takeUntil(this.unsubscribe)
         ).subscribe(() => this.updateError())
     }
